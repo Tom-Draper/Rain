e60d25137857e90683d3897e744a02caf8fa73f9
Tom Draper <tomjdraper1@gmail.com>
2022-06-04 17:06:15 +0100

    clear terminal

--- rain/src/main.rs
+++ rain/src/main.rs
@@ -1,11 +1,30 @@
+use rand::Rng;
 use terminal_size::{Width, Height, terminal_size};
 
-fn main() {
-    println!("Hello, world!");
+
+fn get_terminal_size() -> (u16, u16) {
     let size = terminal_size();
     if let Some((Width(w), Height(h))) = size {
-        println!("Your terminal is {} cols wide and {} lines tall", w, h);
+        return (w, h);
     } else {
-        println!("Unable to get terminal size");
+        return (0, 0);
     }
+}
+
+fn raindrop(w: u16, h: u16) {
+    println!("Terminal is {} cols wide and {} lines tall", w, h);
+
+    let mut rng = rand::thread_rng();
+    println!("Start: {}, {}", rng.gen_range(0..w), rng.gen_range(1..5));
+    println!("End: {}, {}", rng.gen_range(0..w), rng.gen_range(h-5..h-1));
+}
+
+fn clear_screen() {
+    print!("\x1B[2J\x1B[1;1H");
+}
+
+fn main() {
+    clear_screen();
+    let (w, h) = get_terminal_size();
+    raindrop(w, h);
 }
