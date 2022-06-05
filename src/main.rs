96b4f6f0f0732e67c2b0f85354f3680a2d097b85
Tom Draper <tomjdraper1@gmail.com>
2022-06-05 15:14:28 +0100

    raining sideways

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,32 @@ fn get_terminal_size() -> (u16, u16) {
     }
 }
 
+fn create_left_raindrop(w: u16, h: u16, rng: &mut ThreadRng) -> Raindrop {
+    let mut x: u16 = rng.gen_range(0..w + h);
+    let mut y = 0;
+    // If 
+    if x > w {
+        y = x - w;
+        x = w;
+    }
+    let finish_y = rng.gen_range(h - 3..h - 1);
+
+    return Raindrop { x, y, finish_y };
+}
+
+fn create_right_raindrop(w: u16, h: u16, rng: &mut ThreadRng) -> Raindrop {
+    let mut x: u16 = rng.gen_range(0..w + h);
+    let mut y = 0;
+    // If 
+    if x > w {
+        y = x - w;
+        x = 0;
+    }
+    let finish_y = rng.gen_range(h - 3..h - 1);
+
+    return Raindrop { x, y, finish_y };
+}
+
 fn create_raindrop(w: u16, h: u16, rng: &mut ThreadRng) -> Raindrop {
     let x = rng.gen_range(0..w);
     let y = 0;
@@ -30,6 +56,14 @@ pub struct Raindrop {
     x: u16,
     y: u16,
     finish_y: u16,
+}
+
+fn travelling_right(rng: &mut ThreadRng) {
+    print!("\\");
+}
+
+fn travelling_left(rng: &mut ThreadRng) {
+    print!("/");
 }
 
 fn travelling(rng: &mut ThreadRng) {
@@ -68,10 +102,91 @@ fn landing(rng: &mut ThreadRng) {
     }
 }
 
-
 fn new_raindrops(raindrops: &mut Vec<Raindrop>, w: u16, h: u16, rng: &mut ThreadRng) {
     raindrops.push(create_raindrop(w, h, rng));
     raindrops.push(create_raindrop(w, h, rng));
+}
+
+fn raining_left(w: u16, h: u16) {
+    let cursor: TerminalCursor = cursor();
+    cursor.hide();
+
+    let mut rng = rand::thread_rng();
+    let mut raindrops: Vec<Raindrop> = Vec::new();
+    let mut index: usize;
+    let mut finished_raindrops: VecDeque<usize>;
+    
+    loop {
+        raindrops.push(create_left_raindrop(w, h, &mut rng));
+        raindrops.push(create_left_raindrop(w, h, &mut rng));
+
+        index = 0;
+        finished_raindrops = VecDeque::new();
+        for r in raindrops.iter_mut() {
+            // Remove old raindrop
+            cursor.goto(r.x, r.y);
+            print!(" ");
+            if r.y < r.finish_y && r.x > 0 {
+                // Place moved drop
+                r.y += 1;
+                r.x -= 1;
+                cursor.goto(r.x, r.y);
+                if r.y == r.finish_y {
+                    landing(&mut rng);
+                } else {
+                    travelling_left(&mut rng);
+                }
+            } else {
+                finished_raindrops.push_front(index);
+            }
+            index += 1;
+        }
+
+        for i in finished_raindrops.iter() {
+            raindrops.remove(*i);
+        }
+    }
+}
+
+fn raining_right(w: u16, h: u16) {
+    let cursor: TerminalCursor = cursor();
+    cursor.hide();
+
+    let mut rng = rand::thread_rng();
+    let mut raindrops: Vec<Raindrop> = Vec::new();
+    let mut index: usize;
+    let mut finished_raindrops: VecDeque<usize>;
+    
+    loop {
+        raindrops.push(create_right_raindrop(w, h, &mut rng));
+        raindrops.push(create_right_raindrop(w, h, &mut rng));
+
+        index = 0;
+        finished_raindrops = VecDeque::new();
+        for r in raindrops.iter_mut() {
+            // Remove old raindrop
+            cursor.goto(r.x, r.y);
+            print!(" ");
+            if r.y < r.finish_y && r.x < w {
+                // Place moved drop
+                r.y += 1;
+                r.x += 1;
+                cursor.goto(r.x, r.y);
+                if r.y == r.finish_y {
+                    landing(&mut rng);
+                } else {
+                    travelling_right(&mut rng);
+                }
+            } else {
+                finished_raindrops.push_front(index);
+            }
+            index += 1;
+        }
+
+        for i in finished_raindrops.iter() {
+            raindrops.remove(*i);
+        }
+    }
 }
 
 fn raining(w: u16, h: u16) {
@@ -117,5 +232,15 @@ fn raining(w: u16, h: u16) {
 fn main() {
     clear_screen();
     let (w, h) = get_terminal_size();
-    raining(w, h);
+    
+    let mut rng = rand::thread_rng();
+    let n = rng.gen_range(0..3);
+    if n == 0 {
+        raining(w, h);
+    } else if n == 1 {
+        raining_right(w, h);
+    } else if n == 2 {
+        raining_left(w, h);
+    }
+        
 }
